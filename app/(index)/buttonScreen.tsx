865aca86fd5a1c0342e1be2b7980b8e1bd3171cd
leodebarros <leodebarros89@gmail.com>
2025-02-23 07:50:52 +0000

    button screen

---
+++ app/(index)/buttonScreen.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/leonardoUI/Header";
+import { View } from "@/components/leonardoUI/View";
+
+export default function ButtonScreen() {
+  return (
+    <View>
+      <Header
+        title="Button"
+        description="Trigger actions through our buttons"
+        navBack
+      />
+    </View>
+  );
+}
