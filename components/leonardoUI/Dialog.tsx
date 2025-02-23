44ae67679ab23636516b595afaa8b8e3658b8b05
leodebarros <leodebarros89@gmail.com>
2025-02-23 07:49:43 +0000

    dialog component

---
+++ components/leonardoUI/Dialog.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { View as DefaultView, Modal, StyleSheet } from "react-native";
+import { Button } from "./Button";
+import { useTheme } from "./Theme";
+import { Text } from "./Text";
+
+interface DialogProps {
+  isVisible: boolean;
+  onConfirm?: () => void;
+  onCancel: () => void;
+  onRequestClose?: () => void;
+}
+
+const Dialog: React.FC<DialogProps> = ({
+  isVisible,
+  onConfirm,
+  onCancel,
+  onRequestClose,
+}) => {
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.81)",
+    },
+    modalContent: {
+      width: "80%",
+      padding: theme.padding.lg,
+      backgroundColor: theme.colors.overBackground,
+      borderRadius: theme.borderRadius.md,
+    },
+    modalTitle: {
+      fontWeight: theme.fontWeight.semibold,
+      marginBottom: theme.margin.sm,
+    },
+    modalText: {
+      color: theme.colors.textSecondary,
+      marginBottom: theme.margin.md,
+    },
+  });
+
+  return (
+    <Modal
+      transparent={true}
+      visible={isVisible}
+      animationType="slide"
+      onRequestClose={onRequestClose}
+    >
+      <DefaultView style={styles.modalContainer}>
+        <DefaultView style={styles.modalContent}>
+          <Text weight="semibold" style={styles.modalTitle}>
+            Confirm Action
+          </Text>
+          <Text style={styles.modalText}>
+            Are you sure you want to perform this action?
+          </Text>
+          <DefaultView>
+            <Button
+              caption="Confirm"
+              onPress={onConfirm}
+              type="primary"
+              style={{ marginBottom: 0 }}
+            />
+            <Button caption="Cancel" onPress={onCancel} type="outline" />
+          </DefaultView>
+        </DefaultView>
+      </DefaultView>
+    </Modal>
+  );
+};
+
+export default Dialog;
