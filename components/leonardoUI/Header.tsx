e9043e24447cd98beb141c314227454ddbf9d86c
leodebarros <leodebarros89@gmail.com>
2025-02-23 07:49:20 +0000

    header component

---
+++ components/leonardoUI/Header.tsx
@@ -0,0 +1,61 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "./Theme";
+import { useRouter } from "expo-router";
+import { Text } from "./Text";
+
+interface HeaderProps {
+  title: string;
+  description?: string;
+  navBack?: boolean;
+}
+
+export default function Header({
+  title,
+  description,
+  navBack = false,
+}: HeaderProps) {
+  const theme = useTheme();
+  const router = useRouter();
+
+  const handleBackPress = () => {
+    router.back();
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      maxWidth: "100%",
+      gap: 3,
+      marginTop: navBack ? 0 : theme.margin.lg,
+      marginBottom: theme.margin.xs,
+    },
+    description: {
+      marginBottom: theme.margin.md,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      {navBack && (
+        <Pressable onPress={handleBackPress}>
+          <Ionicons
+            name="arrow-back-outline"
+            size={24}
+            color={theme.colors.textPrimary}
+          />
+        </Pressable>
+      )}
+      <Text size="xl" weight="bold">
+        {title}
+      </Text>
+      {description && (
+        <View>
+          <Text color="textSecondary" style={styles.description}>
+            {description}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
