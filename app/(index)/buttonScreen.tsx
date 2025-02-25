608b38e9035f39f738ab1635f7f137bd6b935ff4
leodebarros <leodebarros89@gmail.com>
2025-02-25 13:46:20 +0000

    button screen

--- app/(index)/buttonScreen.tsx
+++ app/(index)/buttonScreen.tsx
@@ -1,14 +1,27 @@
+import { Button, ButtonType } from "@/components/leonardoUI/Button";
+import Card from "@/components/leonardoUI/Card";
 import Header from "@/components/leonardoUI/Header";
 import { View } from "@/components/leonardoUI/View";
+import { BUTTON_VARIANTS } from "@/data/components";
 
 export default function ButtonScreen() {
   return (
     <View>
       <Header
         title="Button"
-        description="Trigger actions through our buttons"
+        description="A tappable UI element that triggers an action or navigation when pressed, supporting multiple styles and states."
         navBack
       />
+
+      {BUTTON_VARIANTS.map(({ title, description, type }) => (
+        <Card key={type}>
+          <Card.Title>{title}</Card.Title>
+          <Card.Description>{description}</Card.Description>
+          <Button caption="Enabled" type={type as ButtonType} />
+          <Button caption="Disabled" type={type as ButtonType} disabled />
+          <Button caption="Loading" type={type as ButtonType} loading />
+        </Card>
+      ))}
     </View>
   );
 }
