c8ec8b9557399c0866c3c7f26c05c723be3ffae5
leodebarros <leodebarros89@gmail.com>
2025-02-25 13:46:54 +0000

    incomplete dialog screen

---
+++ app/(index)/dialogScreen.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { View as DefaultView, ScrollView, StyleSheet } from "react-native";
+import { Button } from "@/components/leonardoUI/Button";
+import Card from "@/components/leonardoUI/Card";
+import { View } from "@/components/leonardoUI/View";
+import Header from "@/components/leonardoUI/Header";
+import Dialog from "@/components/leonardoUI/Dialog";
+
+export default function DialogScreen() {
+  const [showPrimaryDialog, setShowPrimaryDialog] = useState<boolean>(false);
+  const [showDefaultDialog, setShowDefaultDialog] = useState<boolean>(false);
+
+  return (
+    <View>
+      <Header title="Dialog" description="Prompt for double check" navBack />
+      <DefaultView style={{ gap: 12 }}>
+        <Card>
+          <Card.Title>Dialog</Card.Title>
+          <Card.Description>Type: primary</Card.Description>
+          <Card.Footer
+            button={
+              <Button
+                caption="Open dialog"
+                type="primary"
+                onPress={() => setShowPrimaryDialog(true)}
+              />
+            }
+          />
+        </Card>
+        <Dialog
+          isVisible={showPrimaryDialog}
+          onCancel={() => setShowPrimaryDialog(false)}
+        />
+        <Card>
+          <Card.Title>Dialog</Card.Title>
+          <Card.Description>Type: default</Card.Description>
+          <Card.Footer
+            button={
+              <Button
+                caption="Open dialog"
+                type="default"
+                onPress={() => setShowDefaultDialog(true)}
+              />
+            }
+          />
+        </Card>
+        <Dialog
+          type="default"
+          isVisible={showDefaultDialog}
+          onCancel={() => setShowDefaultDialog(false)}
+        />
+      </DefaultView>
+    </View>
+  );
+}
