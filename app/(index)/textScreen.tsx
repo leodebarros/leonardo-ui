b355bb98418745b0e84f1fd962c5672f56fa7b27
leodebarros <leodebarros89@gmail.com>
2025-02-25 18:54:15 +0000

    improves text screen

--- app/(index)/textScreen.tsx
+++ app/(index)/textScreen.tsx
@@ -1,47 +1,65 @@
 import Card from "@/components/leonardoUI/Card";
 import Header from "@/components/leonardoUI/Header";
-import { Text } from "@/components/leonardoUI/Text";
+import { FontWeight, Text, TextSize } from "@/components/leonardoUI/Text";
 import { View } from "@/components/leonardoUI/View";
-import { View as RNView } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
+import { TEXT_VARIANTS } from "@/data/components";
+import { useState } from "react";
+import Filter from "@/components/leonardoUI/Filter";
+import { useTheme } from "@/store/themeContext";
 
 export default function TextScreen() {
+  const theme = useTheme();
+  const filterOptions = [
+    { label: "normal" },
+    { label: "semibold" },
+    { label: "bold" },
+  ];
+
+  const [activeFilter, setActiveFilter] = useState<string>("normal");
+
+  const styles = StyleSheet.create({
+    scrollContainer: {
+      paddingVertical: theme.padding.sm,
+      marginBottom: theme.margin.md,
+    },
+  });
+
   return (
     <View>
       <Header
         title="Text"
-        description="Elegant, simple and conveying text"
+        description="A configurable component for displaying and styling text consistently throughout the app."
         navBack
       />
-      <Card>
-        <Card.Title>xs</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="xs">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
-      <Card>
-        <Card.Title>sm</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="sm">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
-      <Card>
-        <Card.Title>base</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="base">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
-      <Card>
-        <Card.Title>md</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="md">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
-      <Card>
-        <Card.Title>lg</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="lg">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
-      <Card>
-        <Card.Title>xl</Card.Title>
-        <Card.Description>normal</Card.Description>
-        <Text size="xl">The quick brown fox jumps over the lazy dog.</Text>
-      </Card>
+      <Text color="textSidebar" weight="semibold">
+        Font Weight
+      </Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.scrollContainer}
+      >
+        {filterOptions.map((option) => (
+          <Filter
+            key={option.label}
+            type="default"
+            label={option.label}
+            isActive={option.label === activeFilter}
+            onPress={() => setActiveFilter(option.label)}
+          />
+        ))}
+      </ScrollView>
+
+      {TEXT_VARIANTS.map(({ size }, index) => (
+        <Card key={index}>
+          <Card.Title>{size}</Card.Title>
+          <Card.Description>{activeFilter}</Card.Description>
+          <Text size={size as TextSize} weight={activeFilter as FontWeight}>
+            The quick brown fox jumps over the lazy dog.
+          </Text>
+        </Card>
+      ))}
     </View>
   );
 }
